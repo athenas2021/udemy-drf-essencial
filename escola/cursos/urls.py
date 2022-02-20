3eb177a3a844ce69ea4716f189aa06c63ce8e655
athenas2021 <lucas.kano@outlook.com>
2022-02-20 12:15:31 -0300

    Sobrescrito métodos get

--- escola/cursos/urls.py
+++ escola/cursos/urls.py
@@ -3,8 +3,10 @@ from .views import CursoAPIView, AvaliacaoAPIView, CursosAPIView, AvaliacoesAPIV
 
 urlpatterns = [
     path('curso/<int: pk>/', CursoAPIView.as_view(), name='curso'),
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacao/<int: pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int: curso_pk>/avaliacoes', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int: curso_pk>/avaliacoes/<int: avalicao_pk>/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+    path('avaliacao/<int: avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
 
 ]
--- escola/cursos/view.py
+++ escola/cursos/view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from .models import Curso, Avaliacao
@@ -15,6 +16,17 @@ class AvaliacoesAPIView(generics.ListAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 class AvaliacaoAPIView(generics.RetrieveDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+            pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
